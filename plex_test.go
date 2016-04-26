2c0a023c73c107e19f1950b41beae7e369bd8ad0
Daniel Jones <daniel.jones@engineerbetter.com>
2016-04-26 10:58:16 +0100

    call command with env

--- plex_test.go
+++ plex_test.go
@@ -4,25 +4,40 @@ import (
 	. "github.com/EngineerBetter/cf-plex"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"os"
+	"os/exec"
 )
 
-var _ = Describe("SetEnv", func() {
-	Context("when the env var is already set", func() {
-		It("replaces the value", func() {
-			env := []string{"KEY=value", "CF_HOME=foo"}
-			env = SetEnv("CF_HOME", "bar", env)
-			Ω(env).Should(ContainElement("CF_HOME=bar"))
-			Ω(env).Should(ContainElement("KEY=value"))
-			Ω(env).ShouldNot(ContainElement("CF_HOME=foo"))
+var _ = Describe("cf-plex", func() {
+	Describe("SetEnv", func() {
+		Context("when the env var is already set", func() {
+			It("replaces the value", func() {
+				env := []string{"KEY=value", "CF_HOME=foo"}
+				env = SetEnv("CF_HOME", "bar", env)
+				Ω(env).Should(ContainElement("CF_HOME=bar"))
+				Ω(env).Should(ContainElement("KEY=value"))
+				Ω(env).ShouldNot(ContainElement("CF_HOME=foo"))
+			})
+		})
+
+		Context("when the env var is not set already", func() {
+			It("adds the value", func() {
+				env := []string{"KEY=value"}
+				env = SetEnv("CF_HOME", "bar", env)
+				Ω(env).Should(ContainElement("CF_HOME=bar"))
+				Ω(env).Should(ContainElement("KEY=value"))
+			})
 		})
 	})
 
-	Context("when the env var is not set already", func() {
-		It("adds the value", func() {
-			env := []string{"KEY=value"}
-			env = SetEnv("CF_HOME", "bar", env)
-			Ω(env).Should(ContainElement("CF_HOME=bar"))
-			Ω(env).Should(ContainElement("KEY=value"))
-		})
+	It("calls external things", func() {
+		env := os.Environ()
+		cmd := exec.Command("cf", "api")
+		cmd.Env = SetEnv("CF_HOME", "/tmp", env)
+		bytes, err := cmd.Output()
+		output := string(bytes)
+		Ω(err).ShouldNot(HaveOccurred())
+		Ω(output).Should(Equal("No api endpoint set. Use 'cf api' to set an endpoint\n"))
 	})
 })
