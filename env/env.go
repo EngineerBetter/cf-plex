c784bc9a88464d86d5935f804697564ade739174
Daniel Jones <daniel.jones@engineerbetter.com>
2016-05-06 17:01:53 +0100

    error handling for GetCoordinate

--- env/env.go
+++ env/env.go
@@ -1,6 +1,7 @@
 package env
 
 import (
+	"errors"
 	"strings"
 )
 
@@ -10,25 +11,32 @@ type Coord struct {
 	Api      string
 }
 
-func GetCoordinates(cfEnvs string) []Coord {
+func GetCoordinates(cfEnvs string) ([]Coord, error) {
 	var coords []Coord
 	triples := GetTriples(cfEnvs)
 	for _, triple := range triples {
-		coords = append(coords, GetCoordinate(triple))
+		coord, err := GetCoordinate(triple)
+		if err != nil {
+			return nil, err
+		}
+		coords = append(coords, coord)
 	}
-	return coords
+	return coords, nil
 }
 
 func GetTriples(cfEnvs string) []string {
 	return strings.Split(cfEnvs, ";")
 }
 
-func GetCoordinate(triple string) Coord {
+func GetCoordinate(triple string) (coord Coord, err error) {
+	if strings.Count(triple, "@") != 1 || strings.Count(triple, ":") != 1 {
+		return coord, errors.New(triple + " is invalid")
+	}
 	credsAndApi := strings.Split(triple, "@")
 	creds := strings.Split(credsAndApi[0], ":")
 	username := creds[0]
 	password := creds[1]
 	api := credsAndApi[1]
 
-	return Coord{Username: username, Password: password, Api: api}
+	return Coord{Username: username, Password: password, Api: api}, err
 }
--- env/env_test.go
+++ env/env_test.go
@@ -10,10 +10,18 @@ var _ = Describe("env", func() {
 	Describe("getCoordinate", func() {
 		It("returns coordinate for an API", func() {
 			cfEnv := "username:password@api.com"
-			coords := GetCoordinate(cfEnv)
+			coords, err := GetCoordinate(cfEnv)
+			Ω(err).ShouldNot(HaveOccurred())
 			Ω(coords.Username).Should(Equal("username"))
 			Ω(coords.Password).Should(Equal("password"))
 			Ω(coords.Api).Should(Equal("api.com"))
+		})
+
+		It("returns an error for invalid values", func() {
+			cfEnv := "username:password"
+			_, err := GetCoordinate(cfEnv)
+			Ω(err).Should(HaveOccurred())
+			Ω(err).Should(MatchError("username:password is invalid"))
 		})
 	})
 
@@ -29,7 +37,8 @@ var _ = Describe("env", func() {
 	Describe("GetCoordinates", func() {
 		It("returns coordinates for many APIs", func() {
 			cfEnvs := "user1:pass1@api1.com;user2:pass2@api2.com"
-			coords := GetCoordinates(cfEnvs)
+			coords, err := GetCoordinates(cfEnvs)
+			Ω(err).ShouldNot(HaveOccurred())
 			Ω(coords[0]).Should(Equal(Coord{Username: "user1", Password: "pass1", Api: "api1.com"}))
 			Ω(coords[1]).Should(Equal(Coord{Username: "user2", Password: "pass2", Api: "api2.com"}))
 		})
